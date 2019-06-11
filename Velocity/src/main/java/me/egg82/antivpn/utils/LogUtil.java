b73d6328d5d7d6126b63c8c99ff94e466bb42933
egg82 <phantom_zero@ymail.com>
2019-06-11 14:35:02 -0600

    Minor annoyance fixes

--- Velocity/src/main/java/me/egg82/antivpn/utils/LogUtil.java
+++ Velocity/src/main/java/me/egg82/antivpn/utils/LogUtil.java
@@ -4,6 +4,8 @@ import net.kyori.text.TextComponent;
 import net.kyori.text.format.TextColor;
 
 public class LogUtil {
+    private LogUtil() {}
+
     public static TextComponent.Builder getHeading() {
         return TextComponent.builder("[").color(TextColor.YELLOW)
                 .append(TextComponent.of("Anti-VPN").color(TextColor.AQUA))
