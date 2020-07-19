a76366c925158a4b016f2b8d9b63ab7ef66d31e3
Larry <hey@laarryy.dev>
2020-07-19 16:53:59 -0600

    oops a bad autoimport

--- Bukkit/src/main/java/me/egg82/antivpn/hooks/VaultHook.java
+++ Bukkit/src/main/java/me/egg82/antivpn/hooks/VaultHook.java
@@ -13,8 +13,6 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
-
 public class VaultHook implements PluginHook {
 
     public final Logger logger = LoggerFactory.getLogger(getClass());
