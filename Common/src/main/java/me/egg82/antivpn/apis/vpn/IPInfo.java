9e942bb1ed6bde1157ecfbe0e388f7c009c8adbb
Larry <hey@laarryy.dev>
2020-07-20 11:38:14 -0600

    add Seb's suggestion

--- Common/src/main/java/me/egg82/antivpn/apis/vpn/IPInfo.java
+++ Common/src/main/java/me/egg82/antivpn/apis/vpn/IPInfo.java
@@ -1,5 +1,6 @@
 package me.egg82.antivpn.apis.vpn;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import me.egg82.antivpn.APIException;
 import me.egg82.antivpn.utils.ValidationUtil;
 import ninja.egg82.json.JSONWebUtil;
@@ -11,9 +12,21 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class IPInfo extends AbstractSourceAPI {
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    private static AtomicInteger hourlyRequests = new AtomicInteger(0);
+    private static AtomicInteger minuteRequests = new AtomicInteger(0);
+    private static ScheduledExecutorService threadPool = Executors.newSingleThreadScheduledExecutor(new ThreadFactoryBuilder().setNameFormat("AntiVPN-IPInfoAPI-%d").build());
+
+    static {
+        threadPool.scheduleAtFixedRate(() -> hourlyRequests.set(0), 0L, 24L, TimeUnit.HOURS);
+        threadPool.scheduleAtFixedRate(() -> minuteRequests.set(0), 0L, 1L, TimeUnit.MINUTES);
+    }
 
     public String getName() { return "ipinfo"; }
 
@@ -33,6 +46,14 @@ public class IPInfo extends AbstractSourceAPI {
         if (key == null || key.isEmpty()) {
             throw new APIException(true, "Key is not defined for " + getName());
         }
+
+        if (hourlyRequests.getAndIncrement() >= 500) {
+            throw new APIException(true, "API calls to this source have been limited to 500/day as per request.");
+        }
+        if (minuteRequests.getAndIncrement() >= 15) {
+            throw new APIException(true, "API calls to this source have been limited to 15/minute as per request.");
+        }
+
         JSONObject json;
         try {
             json = JSONWebUtil.getJSONObject(new URL("https://ipinfo.io/" + ip + "/privacy?token=" + key), "GET", (int) getCachedConfig().getTimeout());
