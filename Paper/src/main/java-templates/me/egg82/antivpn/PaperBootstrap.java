d2cffee7e0c30f9cccbbaf799ddda4b1c88a54b8
egg82 <eggys82@gmail.com>
2021-03-21 13:06:46 -0600

    Few updates in dep injections

--- Paper/src/main/java-templates/me/egg82/antivpn/PaperBootstrap.java
+++ Paper/src/main/java-templates/me/egg82/antivpn/PaperBootstrap.java
@@ -3,12 +3,13 @@ package me.egg82.antivpn;
 import me.egg82.antivpn.api.platform.Platform;
 import me.egg82.antivpn.config.ConfigUtil;
 import me.egg82.antivpn.config.ConfigurationFileUtil;
-import me.egg82.antivpn.logging.GELFLogger;
 import me.egg82.antivpn.logging.GELFLoggerUtil;
 import me.egg82.antivpn.messaging.ServerIDUtil;
 import ninja.egg82.mvn.JarBuilder;
 import ninja.egg82.mvn.JarInjector;
 import ninja.egg82.mvn.RelocationBuilder;
+import ninja.egg82.mvn.classloaders.InjectableClassLoader;
+import ninja.egg82.mvn.classloaders.TransparentInjectableClassLoader;
 import org.apache.maven.model.building.ModelBuildingException;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -43,7 +44,7 @@ public class PaperBootstrap extends JavaPlugin {
 
         try {
             Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
-            loadJars(new File(getDataFolder(), "cache"), (URLClassLoader) getClass().getClassLoader());
+            loadJars(new File(getDataFolder(), "cache"), new TransparentInjectableClassLoader((URLClassLoader)getClass().getClassLoader()));
         } catch (ClassCastException | IOException | ModelBuildingException ex) {
             Thread.currentThread().setContextClassLoader(origClassLoader);
             if (getAllowErrorStats()) {
@@ -109,13 +110,11 @@ public class PaperBootstrap extends JavaPlugin {
         }
     }
 
-    private void loadJars(@NotNull File cacheDir, @NotNull URLClassLoader parentLoader) throws IOException, ModelBuildingException {
-        Logger builderLogger = new GELFLogger(LoggerFactory.getLogger(JarBuilder.class));
-
-        JarInjector injector = new JarInjector(cacheDir, new GELFLogger(LoggerFactory.getLogger(JarInjector.class)));
+    private void loadJars(@NotNull File cacheDir, @NotNull InjectableClassLoader parentLoader) throws IOException, ModelBuildingException {
+        JarInjector injector = new JarInjector(cacheDir);
 
         // Caffeine
-        injector.addBuilder(new JarBuilder("com.github.ben-manes.caffeine", "caffeine", "${caffeine.version}", builderLogger)
+        injector.addBuilder(new JarBuilder("com.github.ben-manes.caffeine", "caffeine", "${caffeine.version}")
                                     .setRepositoryProxy("https://repo.maven.apache.org/maven2", "https://nexus.egg82.me/repository/maven-central/"));
         injector.addRelocation(new RelocationBuilder("com{}github{}benmanes{}caffeine", "me.egg82.antivpn.external").build("{}", "."));
 
@@ -123,12 +122,12 @@ public class PaperBootstrap extends JavaPlugin {
         try {
             Class.forName("com.github.luben.zstd.Zstd");
         } catch (ClassNotFoundException ignored) {
-            injector.addBuilder(new JarBuilder("com.github.luben", "zstd-jni", "${zstd.version}", builderLogger)
+            injector.addBuilder(new JarBuilder("com.github.luben", "zstd-jni", "${zstd.version}")
                                         .setRepositoryProxy("https://repo.maven.apache.org/maven2", "https://nexus.egg82.me/repository/maven-central/"));
         }
 
         // IPAddress
-        injector.addBuilder(new JarBuilder("com.github.seancfoley", "ipaddress", "${ipaddress.version}", builderLogger)
+        injector.addBuilder(new JarBuilder("com.github.seancfoley", "ipaddress", "${ipaddress.version}")
                                     .setRepositoryProxy("https://repo.maven.apache.org/maven2", "https://nexus.egg82.me/repository/maven-central/"));
         injector.addRelocation(new RelocationBuilder("inet{}ipaddr", "me.egg82.antivpn.external").build("{}", "."));
 
@@ -136,7 +135,7 @@ public class PaperBootstrap extends JavaPlugin {
         try {
             Class.forName("org.h2.Driver");
         } catch (ClassNotFoundException ignored) {
-            injector.addBuilder(new JarBuilder("com.h2database", "h2", "${h2.version}", builderLogger)
+            injector.addBuilder(new JarBuilder("com.h2database", "h2", "${h2.version}")
                                         .setRepositoryProxy("https://repo.maven.apache.org/maven2", "https://nexus.egg82.me/repository/maven-central/"));
         }
 
@@ -144,24 +143,24 @@ public class PaperBootstrap extends JavaPlugin {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException ignored) {
-            injector.addBuilder(new JarBuilder("mysql", "mysql-connector-java", "${mysql.version}", builderLogger)
+            injector.addBuilder(new JarBuilder("mysql", "mysql-connector-java", "${mysql.version}")
                                         .setRepositoryProxy("https://repo.maven.apache.org/maven2", "https://nexus.egg82.me/repository/maven-central/"));
         }
 
         // RabbitMQ
-        injector.addBuilder(new JarBuilder(replace("com{}rabbitmq"), "amqp-client", "${rabbitmq.version}", builderLogger)
+        injector.addBuilder(new JarBuilder(replace("com{}rabbitmq"), "amqp-client", "${rabbitmq.version}")
                                     .setRepositoryProxy("https://repo.maven.apache.org/maven2", "https://nexus.egg82.me/repository/maven-central/"));
         injector.addRelocation(new RelocationBuilder("com{}rabbitmq", "me.egg82.antivpn.external").build("{}", "."));
 
         // Ebean
-        injector.addBuilder(new JarBuilder(replace("io{}ebean"), "ebean-core", "${ebean.version}", builderLogger)
+        injector.addBuilder(new JarBuilder(replace("io{}ebean"), "ebean-core", "${ebean.version}")
                                     .setRepositoryProxy("https://repo.maven.apache.org/maven2", "https://nexus.egg82.me/repository/maven-central/"));
         injector.addRelocation(new RelocationBuilder("io{}ebean", "me.egg82.antivpn.external").build("{}", "."));
         injector.addRelocation(new RelocationBuilder("io{}ebeaninternal", "me.egg82.antivpn.external").build("{}", "."));
         injector.addRelocation(new RelocationBuilder("io{}ebeanservice", "me.egg82.antivpn.external").build("{}", "."));
 
         // Javassist
-        injector.addBuilder(new JarBuilder("org.javassist", getJavassistPackage(), "${javassist.version}", builderLogger)
+        injector.addBuilder(new JarBuilder("org.javassist", getJavassistPackage(), "${javassist.version}")
                                     .setRepositoryProxy("https://repo.maven.apache.org/maven2", "https://nexus.egg82.me/repository/maven-central/"));
         injector.addRelocation(new RelocationBuilder(getJavassistPackage(), "me.egg82.antivpn.external").build());
 
@@ -169,7 +168,7 @@ public class PaperBootstrap extends JavaPlugin {
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException ignored) {
-            injector.addBuilder(new JarBuilder("org.postgresql", "postgresql", "${postgresql.version}", builderLogger)
+            injector.addBuilder(new JarBuilder("org.postgresql", "postgresql", "${postgresql.version}")
                                         .setRepositoryProxy("https://repo.maven.apache.org/maven2", "https://nexus.egg82.me/repository/maven-central/"));
         }
 
@@ -177,12 +176,12 @@ public class PaperBootstrap extends JavaPlugin {
         try {
             Class.forName("org.sqlite.JDBC");
         } catch (ClassNotFoundException ignored) {
-            injector.addBuilder(new JarBuilder("org.xerial", "sqlite-jdbc", "${sqlite.version}", builderLogger)
+            injector.addBuilder(new JarBuilder("org.xerial", "sqlite-jdbc", "${sqlite.version}")
                                         .setRepositoryProxy("https://repo.maven.apache.org/maven2", "https://nexus.egg82.me/repository/maven-central/"));
         }
 
         // Jedis
-        injector.addBuilder(new JarBuilder("redis.clients", "jedis", "${jedis.version}", builderLogger)
+        injector.addBuilder(new JarBuilder("redis.clients", "jedis", "${jedis.version}")
                                     .setRepositoryProxy("https://repo.maven.apache.org/maven2", "https://nexus.egg82.me/repository/maven-central/"));
         injector.addRelocation(new RelocationBuilder("redis{}clients{}jedis", "me.egg82.antivpn.external").build("{}", "."));
 
